85828a0c78a8a08528772011c2b0ae127a71e3ac
Renato Groffe <renato.groffe@yahoo.com.br>
2024-11-11 07:50:56 -0300

    Commit inicial
    Commit inicial

---
+++ src/ConsoleAppDebugAssert/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("***** Testes com .NET 9 | Melhorias em Debug.Assert *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+Console.WriteLine();
+Console.WriteLine("Informe um numero inteiro positivo:");
+var input = Console.ReadLine();
+
+Debug.Assert(int.TryParse(input, out var number) && number > 0);
+
+Console.WriteLine();
+Console.WriteLine("Debug.Assert() nao produziu falha...");
